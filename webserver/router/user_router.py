4c9c1d18526ff3a48cc0729a6b1781e3e4fe4c77
MajorX234 <majorx234@googlemail.com>
2026-04-28 04:54:09 +0200

    Add endpoint for database query

--- webserver/router/user_router.py
+++ webserver/router/user_router.py
@@ -2,10 +2,19 @@ from fastapi import APIRouter, Request
 from fastapi.responses import JSONResponse
 from database import DatabaseInterface
 #from models.user import User
+from sqlmodel import select, Field, SQLModel
+
+
+class Users(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    email: str
+    birth_date: str
 
 
 class UserRouter:
     def __init__(self, db: DatabaseInterface):
+        self.db = db
         self._router = APIRouter(
             prefix="/user",
             tags=['user'],
@@ -15,6 +24,11 @@ class UserRouter:
             self.get_users,
             methods=["GET"]
         )
+        self._router.add_api_route(
+            "/get_users_db",
+            self.get_users_db,
+            methods=["GET"]
+        )
 
     def get_router(self):
         return self._router
@@ -22,4 +36,10 @@ class UserRouter:
     def get_users(self, request: Request):
         return JSONResponse(
                 content={"users": ["Torben", "Michel", "Tommy", "Annika"]},
+                status_code=200)
+
+    def get_users_db(self, request: Request):
+        user_list = self.db.query(select(Users.name))
+        return JSONResponse(
+                content={"users": user_list},
                 status_code=200)
